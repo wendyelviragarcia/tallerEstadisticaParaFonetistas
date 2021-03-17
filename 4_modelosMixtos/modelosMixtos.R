7252a8f017a709f0dee9434823530aaece13663f
wendyelviragarcia <wendyelviragarcia@gmail.com>
2021-03-17 19:53:51 +0100

    Update modelosMixtos.R

--- 4_modelosMixtos/modelosMixtos.R
+++ 4_modelosMixtos/modelosMixtos.R
@@ -15,22 +15,21 @@ setwd("/Users/weg/OneDrive - UNED/git-me/tallerEstadisticaParaFonetistas/4_model
 allDur <- read.csv("consonantDuration.csv")
 
 
-allDur$Sexo <- allDur$Gender
-dfAllDurC <- allDur[allDur$Tipo=="Consonante",]
-
-
 shapiro.test(allDur$dur)
 
 
 #duraciones no normales
 
+# comprobamos varianzas, ya ni haría falta
 leveneTest(allDur$dur,allDur$Grupo)
-# que paso???
+# que paso??? Lee el warning
 
 library("dplyr")
 allDur <- allDur %>% mutate_if(is.character,as.factor)
 
-
+###################
+# COMPROBACION NORMALIDAD
+#############
 
 library(nortest)
 lillie.test(dfAllDurC$dur)
@@ -54,31 +53,52 @@ ggplot(data = allDur,aes(x=Grupo,y=dur,fill=Grupo, colour=Sujeto))+geom_boxplot(
   ggtitle("Duración")+theme_classic()+ylab("(ms)")+xlab("")
 #ggsave("duracion.tiff", units="cm", width=15, height=15, dpi=300, compression = 'lzw')
 
+
+
 ###################
-# COMPROBACION NORMALIDAD
+# MODELO LINEAL DURACION
 #############
-shapiro.test(dfAllDurV$dur)
-ks.test(dfAllDurV$dur,pnorm,mean(dfAllDurV$dur),sd(dfAllDurV$dur))
+# la idea para montarlo es como una anova
+# la duración depende del grupo?
+# pero en vez de devolvernos si sí o no (si pueden ser parte de la misma muestra)
+# nos dice cuanto cambian los números dependiendo del grupo
+# a esto se le llama regresión y es como las fórmulitas aquellas del cole 
+# en las que sacábamos la función de una pendiente
 
-bartlett.test(dfAllDurC$dur,dfAllDurC$Grupo)
-bartlett.test(dfAllDurC$dur,dfAllDurC$Control)
+modeloLineal <- lm(dur ~ Grupo, data=allDur)
+summary(modeloLineal)
 
-
-shapiro.test(dfAllDurV$dur)
-###################
-# MODELO DURACION
-#############
+# pero la regresión ha existido toda la vida no?
+# Sí, nuestro objetivo no son los factores fijos, son los aleatorios
+# necesarios para experimentos con medidas repetidas (3 repes de algo)
+# o donde esperamos que cada sujeto tenga su propia media
+# por ejemplo, yo sé que cada sujeto tendrá un F0 base propio y a partir de ahí
+# esperaré las variaciones, no me importa si uno tiene 120 y el otro 220 me importa
+# si a su base le están haciendo +60 o -60.
+# Incluir el random effect da cuenta de esa variación
+# Incluimos que cada sujeto tiene una duración diferente
+# (Otra cosa posible es normalizar, en z-score o en este caso con su speech rate)
 
 modelo <- lmer(dur ~ Grupo+ Tipo+ (1|Sujeto), data=allDur)
 summary(modelo)
 
-#EL INCLUIDO EN EL PAPER con INTERACCION 
-modeloConInteraccion <- lmer(dur ~ Grupo+Tipo+ (1+type|Sujeto), data=allDur)
-coef(modeloConInteraccion)
-summary(modeloConInteraccion) # 754.0    27.46    
 
-# Y cómo sabemos cuál es mejor?
-# Calculamos el conditional Akaike information
+# EL INCLUIDO EN EL PAPER con INTERACCIÓN
+# Un pasito más allá, a veces los sujetos pueden interaccionar, pueden no "sumarle lo mismo" según
+# si algo es vocal o consonante. Igual en las consonantes suman más duración
+modeloConInteraccion <- lmer(dur ~ Grupo+Tipo+ (1+Tipo|Sujeto), data=allDur)
+coef(modeloConInteraccion)
+summary(modeloConInteraccion) # 754.0    27.46
+
+################
+# ¿Y cómo sé qué modelo incluir en el paper?
+################
+# Inlcuimos el mejor, el que da mejor cuenta de la variación de los datos
+# ¿Y cómo sabemos cuál es mejor?
+# De muchas maneras, os enseño 2 aptas para filólogos
+
+# 1) Residuo pequeño y normal
+# 2) AIC Calculamos el conditional Akaike information
 cAIC(modelo) #29728.90
 cAIC(modeloConInteraccion) # 29380.73
 # Y para saber cuál es mejor, calculamos la diferencia entre 2 y quiere decir que...
@@ -98,9 +118,10 @@ cAIC(modeloConInteraccion) # 29380.73
 # Vamos a comparar el modelo con el grupo (APPsem, Control) contra un modelo
 # que no incluyera esa variable. Después comparamos los modelos con una 
 # anova para ver si son iguales o diferentes. Si son diferentes, quiere
-# decir que esa variable hace que el modelo cambie, es significativo
+# decir que esa variable hace que el modelo cambie, esa variable es significativa
 
-# Modelo para hacer la anova necesita el REML en false
+# Para hacer la anova de un modelo hay que poner el REML en false
+# ¿Por qué? No lo sé. Pero a los estadísticos se les hace caso
 modelo <- lmer(dur ~ Grupo+Tipo+ (1+Tipo|Sujeto), REML = FALSE, data=allDur)
 summary(modelo)
 modelo.null <- lmer(dur ~ Tipo+ (1+Tipo|Sujeto), REML = FALSE, data=allDur)
@@ -108,15 +129,32 @@ anova(modelo,modelo.null)
 
 
 # Y ahora, cuáles de los grupos tienen diferencias
+#######
+# POSIBILIDADES DE POST-HOC PARA UN MODELO
+######
+# volvemos a crearnos el modelo
 modelo <- lmer(dur ~ Grupo+Tipo+ (1+Tipo|Sujeto), REML = TRUE, data=allDur)
 
+# vamos a hipotetizar que nuestros datos se ajustan al modelo
 testHipotesisLineal <- glht(modelo, linfct = mcp(Grupo = "Tukey"))
 summary(testHipotesisLineal)
+# Nos da un warning
+# para arreglarlo tenemos que usar la corrección de bonferroni 
 summary(testHipotesisLineal, test = adjusted(type = "bonferroni"))
 
-
+# O directamente nos vamos a un paquete que han creado específicamente para 
+# hacer post-hocs de lmer y que por tanto está preparado para ello
 # estimated marginal means (EMMs)
-library(emmeans)
-name<-emmeans(modelo, pairwise~Grupo, adjust = "Bonferroni",pbkrtest.limit = 3232)
-pairs(name)
 
+library(emmeans)
+misParejas<-emmeans(modelo, pairwise~Grupo, adjust = "Bonferroni",pbkrtest.limit = 3232)
+pairs(misParejas)
+
+# Atención, hay muchos post-hoc, el mejor para tus datos dependerá de estos
+# y ahí es cuando viene la consulta con el estadístico
+# o, en su defecto, mucho buceo en 1) la información de los paquetes 
+# 2) artículos parecidos al tuyo que hayan pasado peer-review
+# 3) Si tienes la suerte de saber leerlos, ir a los papers donde se explican las 
+# formulas y convertirte en un estadístico de pro, que no estadista.
+
+# Hasta aquí el curso y, go play with data!
